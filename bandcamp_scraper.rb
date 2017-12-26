a16f166b55dd515e78d5e0b5a4a78e572645e5a0
beetlehope <beetlehope@gmail.com>
2017-12-27 01:00:05 +0300

    Partially implemented scraping, created a rails app with postgres

---
+++ bandcamp_scraper.rb
@@ -0,0 +1,104 @@
+require 'mechanize'
+require 'nokogiri'
+require 'active_record'
+
+#connected to db
+ActiveRecord::Base.establish_connection(
+    :adapter  => 'postgresql',
+    :host     => 'localhost',
+    :username => 'bandcamp',
+    :password => 'bandcamp',
+    :database => 'bandcamp_app_development')
+
+#start of scraping
+agent = Mechanize.new
+pages_count = 1
+while pages_count < 2433 do
+    page = agent.get('https://bandcamp.com/artist_index?page=/#{pages_count}')
+    albums_links = page.links_with(href: %r{bandcamp.com$|bandcamp.com/releases$})
+    albums_links = albums_links[0..30]
+
+albums_links.map do |link|
+  album_page = link.click
+
+  license_link =  album_page.at_css("#license.info.license a")
+  license_link_value =  album_page.at_css("#license.info.license a")[:href] if license_link
+
+#building an Album class for saving info into database
+
+class Album < ActiveRecord::Base
+  attr_accessor :license_type
+end
+
+#!!!!when code  @album = Album.new was pasted here, an album was created and saved to the database on each loop run
+
+
+#parsing each opened page to see whether license link is present, if it is, i need to grab info about this album and store it
+if license_link
+
+
+
+
+
+      case license_link_value
+      when "http://creativecommons.org/licenses/by/3.0/"
+        #should save info about this album into database instead of loggin out
+         #puts "Attribution 3.0 International (CC BY 3.0)"
+         @album = Album.new
+         @album.license_type = license_link_value
+         @album.save
+
+      when "http://creativecommons.org/licenses/by-sa/3.0/"
+          #puts "Attribution-ShareAlike 3.0 International (CC BY-SA 3.0)"
+          @album = Album.new
+          @album.license_type = license_link_value
+          @album.save
+
+      when "http://creativecommons.org/licenses/by-nd/3.0/"
+        #puts "Attribution-NoDerivatives 3.0 International (CC BY-ND 3.0)"
+        @album = Album.new
+        @album.license_type = license_link_value
+        @album.save
+
+      when "http://creativecommons.org/licenses/by-nc/3.0/"
+        #puts "Attribution-NonCommercial 3.0 International (CC BY-NC 3.0)"
+        @album = Album.new
+        @album.license_type = license_link_value
+        @album.save
+
+      when "http://creativecommons.org/licenses/by-nc-sa/3.0/"
+        #puts "Attribution-NonCommercial-ShareAlike 3.0 International (CC BY-NC-SA 3.0)"
+        @album = Album.new
+        @album.license_type = license_link_value
+        @album.save
+
+      when "http://creativecommons.org/licenses/by-nc-nd/3.0/"
+        #puts "Attribution-NonCommercial-NoDerivatives 3.0 International (CC BY-NC-ND 3.0)"
+        @album = Album.new
+        @album.license_type = license_link_value
+        @album.save
+      end
+
+else
+    puts "All rights reserved"
+end
+
+end
+
+#!!!Bringt it back at some point
+
+#Implemented random time interval between parsing loops
+#def rand_int(from, to)
+#  rand_in_range(from, to).to_i
+#end
+
+#def rand_in_range(from, to)
+  #rand * (to - from) + from
+#end
+#    sleep(rand_int(10, 50))
+
+  #go to the next page on artists index
+
+    pages_count = pages_count + 1
+
+end
